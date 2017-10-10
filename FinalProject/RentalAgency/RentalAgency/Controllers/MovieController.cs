1dc3fd97a2db2146e250081aae18206b0c6071d9
WilliamParlow <william.nichelatti@catolicasc.org.br>
2017-10-10 18:49:43 -0300

    Movies CRUD complete

--- FinalProject/RentalAgency/RentalAgency/Controllers/MovieController.cs
+++ FinalProject/RentalAgency/RentalAgency/Controllers/MovieController.cs
@@ -1,160 +1,164 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Data.Entity;
-using RentalAgency.Models;
+﻿using RentalAgency.Models;
 using RentalAgency.ViewModels;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace RentalAgency.Controllers {
-    public class MovieController : Controller {
+  public class MovieController : Controller {
 
 
 
 
-        /* 
-         
-            Propriedades da classe 
-         
-        */
-        private ApplicationDbContext _dbContext;
+    /* 
 
+        Propriedades da classe 
 
+    */
+    private ApplicationDbContext _dbContext;
 
 
 
-        /* 
-         
-            Construtores 
-         
-        */
-        public MovieController() {
-            this._dbContext = new ApplicationDbContext();
-            this._dbContext.Costumers.Include(c => c.MembershipType).ToList();
-        }
 
 
+    /* 
 
+        Construtores 
 
+    */
+    public MovieController() {
+      this._dbContext = new ApplicationDbContext();
+      this._dbContext.Costumers.Include(c => c.MembershipType).ToList();
+    }
 
-        /* 
-         
-            Action Results
-         
-        */
-        public ActionResult HomePage() {
 
-            ViewBag.Message = "Home page";
 
-            return View();
-        }
 
-        public ActionResult Movies() {
 
-            var MovieIndexView = new MovieIndexViewModel() {
-                Movies = this._dbContext.Movies.ToList()
-            };
+    /* 
 
-            return View(MovieIndexView.Movies);
-        }
+        Action Results
 
-        public ActionResult Details(int id) {
+    */
 
-            var movies = this._dbContext.Movies.ToList();
 
-            if (id > movies.Count) return HttpNotFound();
+    public ActionResult HomePage() {
 
-            return View(movies.Find(predicted => predicted.Id == id));
+      ViewBag.Message = "Home page";
 
-        }
+      return View();
+    }
 
 
 
+    public ActionResult Movies() {
 
+      var MovieIndexView = new MovieIndexViewModel() {
+        Movies = this._dbContext.Movies.ToList()
+      };
 
+      return View(MovieIndexView.Movies);
+    }
 
 
-        /* 
-         
-            CRUD Actions
-         
-        */
-        public ActionResult Edit(int id) {
 
-            var movie = this._dbContext.Movies.SingleOrDefault(m => m.Id == id);
+    public ActionResult Details(int id) {
 
-            if (movie == null)
-                return HttpNotFound();
+      var movie = this._dbContext.Movies.ToList().Find(predicted => predicted.Id == id);
 
+      if (movie == null) return HttpNotFound();
 
-
-            var movieViewModel = new MovieFormViewModel() {
-                Movie = movie
-            };
-
-
-            return View("FormMovie", movieViewModel);
-
-        }
-
-        public ActionResult New() {
-
-            var movieViewModel = new MovieFormViewModel();
-
-            return View("FormMovie", movieViewModel);
-
-        }
-
-
-        [HttpPost]
-        public ActionResult Delete(int id) {
-
-            var movie = this._dbContext.Movies.SingleOrDefault(m => m.Id == id);
-
-            if (movie != null) {
-
-                this._dbContext.Entry(movie).State = EntityState.Deleted;
-                this._dbContext.SaveChanges();
-
-            }
-
-            return RedirectToAction("Movies");
-
-        }
-
-        [HttpPost] // Will be access just with POST method
-        public ActionResult Save(Movie movie) {
-
-            if (movie.Id != 0) {
-
-                _dbContext.Entry(movie).State = EntityState.Modified;
-
-            } else {
-
-                _dbContext.Movies.Add(movie);
-
-            }
-
-            _dbContext.SaveChanges();
-
-
-            return RedirectToAction("Movies");
-        }
-
-
-
-
-
-
-        /* 
-         
-            Overrides 
-         
-        */
-        protected override void Dispose(bool disposing) {
-            this._dbContext.Dispose();
-        }
+      return View(movie);
 
     }
+
+
+
+
+
+
+
+    /* 
+
+        CRUD Actions
+
+    */
+    public ActionResult Edit(int id) {
+
+      var movie = this._dbContext.Movies.SingleOrDefault(m => m.Id == id);
+
+      if (movie == null)
+        return HttpNotFound();
+
+
+
+      var movieViewModel = new MovieFormViewModel() {
+        Movie = movie
+      };
+
+
+      return View("FormMovie", movieViewModel);
+
+    }
+
+
+
+    public ActionResult New() {
+
+      var movieViewModel = new MovieFormViewModel();
+
+      return View("FormMovie", movieViewModel);
+
+    }
+
+
+
+    [HttpPost] // Will be access just with POST method
+    public ActionResult Save(Movie movie) {
+
+      if (movie.Id == 0) {
+        this._dbContext.Movies.Add(movie);
+      }
+      else {
+        this._dbContext.Entry(movie).State = EntityState.Modified;
+      }
+
+      this._dbContext.SaveChanges();
+
+      return RedirectToAction("Movies");
+    }
+
+
+
+    [HttpPost]
+    public ActionResult Delete(int id) {
+
+      var movie = this._dbContext.Movies.Find(id);
+
+      if (movie != null) {
+
+        this._dbContext.Movies.Remove(movie);
+        this._dbContext.SaveChanges();
+
+      }
+
+      return RedirectToAction("Movies");
+
+    }
+
+
+
+
+
+
+    /* 
+
+        Overrides 
+
+    */
+    protected override void Dispose(bool disposing) {
+      this._dbContext.Dispose();
+    }
+
+  }
 }
