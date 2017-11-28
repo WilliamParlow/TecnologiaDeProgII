fc1da3d2283cc990fdc47de0cc840f775598107c
WilliamParlow <william.nichelatti@catolicasc.org.br>
2017-11-28 21:41:50 -0200

    Creating Customer model - Wainting for userAcc integration

--- FinalProject/RentalAgency/RentalAgency/Models/Costumer.cs
+++ FinalProject/RentalAgency/RentalAgency/Models/Costumer.cs
@@ -1,23 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace RentalAgency.Models {
-    public class Costumer {
+   public class Costumer {
 
-        public int Id { get; set; }
-        public int PurchasedMovies { get; set; }
-        public int PurchasedGames { get; set; }
-        [Required]
-        [StringLength(255)]
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string UserID { get; set; }
-        public bool IsSubscribedToNewsletter { get; set; }
-        public MembershipType MembershipType { get; set; }
-        public byte MembershipTypeId { get; set; }
+      public int Id { get; set; }
 
-    }
+      [Required(ErrorMessage = "Campo nome é obrigatório")]
+      [MaxLength(255)]
+      [Display(Name = "Nome")]
+      public string Name { get; set; }
+
+      [Required(ErrorMessage = "Campo Email é obrigatório")]
+      [MaxLength(255)]
+      [Display(Name = "Email")]
+      public string Email { get; set; }
+
+      [Required(ErrorMessage = "Selecionar um usuário é obrigatório")]
+      [Display(Name = "Usuário")]
+      public string UserID { get; set; }
+
+      [Display(Name = "Inscrever para receber notícias")]
+      public bool IsSubscribedToNewsletter { get; set; }
+
+      [Display(Name = "Membro do usuário")]
+      [ForeignKey("MembershipTypeId")]
+      public MembershipType MembershipType { get; set; }
+
+      [Required(ErrorMessage = "Selecionar um membro é obrigatório")]
+      [Display(Name = "Usuário")]
+      public byte MembershipTypeId { get; set; }
+
+   }
 }
